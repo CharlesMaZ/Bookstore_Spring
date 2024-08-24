8635a00e3ce6144f52b58138c7c90bd1d94c15a1
KAROL <karolma02@gmail.com>
2024-08-24 23:09:04 +0200

    Vite

---
+++ frontend/src/components/PersonInfo.jsx
@@ -0,0 +1,8 @@
+export const PersonInfo = (props) => (
+    <>
+      <h1>{props.person.name}</h1>
+      <h1>{props.person.tel}</h1>
+      <h1>{props.person.location}</h1>
+    </>
+  );
+  
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 // import './index.css'
 
 
+const person = {
+  name: 'John',
+  tel: 25444,
+  location: 'New York'
+}
+
+const person2 = {
+  name: 'John',
+  tel: 2522222,
+  location: 'New York'
+}
 
 
-const element = <div></div>;
-
+const element = 
+  <>
+    {/* <PersonInfo person={person}/> */}
+  </>;
 
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
-  element
+  //<App/>
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  //element
 )
  
